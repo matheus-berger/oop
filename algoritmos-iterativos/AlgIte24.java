af0508f17df0ac8956c51a34456dc48bd132b92f
Matheus Berger <119708800+matheus-berger@users.noreply.github.com>
2024-09-19 11:14:59 -0300

    Add the files of the THIRD LIST of exercises
    List of exercises of iterative algorithms in Java, used to learn and practice the Java language. (second class)

---
+++ algoritmos-iterativos/AlgIte24.java
@@ -0,0 +1,12 @@
+/*
+    24. Fazer um algoritmo que leia números inteiros até que sejam informados dez números perfeitos;
+        Escreva o menor número perfeito informado;
+        Um número perfeito é aquele que é igual a soma dos seus divisores. 
+        Ex: 6=1+2+3 ; 28=1+2+4+7+14
+*/
+
+public class AlgIte24 {
+    public static void main(String[] args) {
+        
+    }
+}
